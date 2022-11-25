1324a01334c58db3c03b266954d4ff3afc51f1ce
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-25 19:19:09 +0900

    feat : init commit

---
+++ src/main/java/com/example/gazaeat/GazaEatApplication.java
@@ -0,0 +1,13 @@
+package com.example.gazaeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GazaEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GazaEatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gazaeat/GazaEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gazaeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GazaEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
