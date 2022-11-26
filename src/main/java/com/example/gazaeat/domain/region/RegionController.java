c81231b7df9de51d494bc73782e84fbfcc0851a9
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-26 13:42:22 +0900

    feat : 이미지 로직 추가

--- src/main/java/com/example/gazaeat/domain/region/RegionController.java
+++ src/main/java/com/example/gazaeat/domain/region/RegionController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.EntityNotFoundException;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,7 +22,14 @@ public class RegionController {
     private final RegionRepository regionRepository;
 
     @PostMapping
-    public String save(RegionEntity region){
+    public String save(RegionEntity region, MultipartFile img) throws IOException {
+
+        String path = System.getProperty("user.dir") + "/src/main/resources/static/files/";
+        UUID uuid = UUID.randomUUID();
+
+        File saveFile = new File(path, uuid.toString());
+        img.transferTo(saveFile);
+        region.setImageUrl("/files/" + uuid.toString());
         regionRepository.save(region);
         return "/";
     }
